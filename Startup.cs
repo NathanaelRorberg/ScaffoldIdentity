c5db99044e658b8d435a13c7eebb0c7ec09fe25d
NathanaelR <nathanael@rorberg.us>
2020-05-18 12:55:34 -0700

    finish basic assignment.

--- Startup.cs
+++ Startup.cs
@@ -48,6 +48,7 @@ namespace ScaffoldIdentity
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
